b9af838f5d80596de239a5e158c863903a8bf7c7
Jacob Shing <zhanhoshing@icloud.com>
2023-07-10 18:44:18 +0800

    removed Console project for renaming

--- ShowControlSystem.Common/ProjectInfo.cs
+++ ShowControlSystem.Common/ProjectInfo.cs
@@ -1,6 +1,6 @@
 ﻿namespace ShowControlSystem.Common;
 
 public static class ProjectInfo {
-    public const string FullVersion = "v0.1-b.1";
+    public const string FullVersion = "v0.1-beta.1";
     public const string AssemblyVersion = "0.1.0.0";
 }
--- ShowControlSystem.Console/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShowControlSystem.Console;
-
-public static class Program {
-    [STAThread]
-    public static int Main(string[] args) {
-        return 0;
-    }
-}
